c898f39d3368bbdb08257b8510221c95663c1042
김성우 <suong22@bizbee.co.kr>
2026-05-14 17:01:10 +0900

    프로젝트 기본 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    basePath: '/',
+    compiler: {
+        styledComponents: true,
+    }
 };
 
 export default nextConfig;
